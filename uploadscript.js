d1f8d4f81e024e38bfb98922cef75ad6711efa4b
misspelled-usernaem <134952931+misspelled-usernaem@users.noreply.github.com>
2023-09-12 16:18:37 +0200

    Update uploadscript.js

--- uploadscript.js
+++ uploadscript.js
@@ -1,27 +1,58 @@
-const posttemp=document.getElementsByClassName('script-container')[0]
 const uploadbtn=document.getElementById('upload')
+
+listScript=async(n,c,d)=>{
+    const posttemp=document.createElement('div')
+    const i1=document.createElement('input')
+    const i2=document.createElement('input')
+    const i3=document.createElement('input')
+    i1.type="button"
+    i2.type="button"
+    i3.type="button"
+    i1.class="script-redirect"
+    i2.class="script-redirect"
+    i3.class="script-redirect"
+
+    posttemp.appendChild(i1)
+    posttemp.appendChild(i2)
+    posttemp.appendChild(i3)
+
+    i1.id="name"
+    i1.value=n
+    
+    i2.id="creator"
+    i2.value=c
+    
+    i3.id="date"
+    i3.value=d
+
+    document.getElementById('s-container').appendChild(posttemp)
+}
+
 createPost=async()=>{
-    //var newPost=posttemp.cloneNode(true)
-    //const c=newPost.childNodes
     const name=document.getElementById('name').value
     //const by=document.getElementById('').value
     const source=document.getElementById('source').value
     const desc=document.getElementById('description').value
 
     const date=new Date
+    const fdate=`${date.getUTCDate()}/${date.getUTCDay()} - ${date.getUTCFullYear()}`
 
-    fetch('./scripts.json')
-    .then(resp=>resp.json())
-    .then(json=>{
-        console.log(json)
-    })
-    
-
-    //c[0].value=name
-    //c[1].value=by
-    //c[2].value=`${date.getUTCDate()}/${date.getUTCDay()} - ${date.getUTCFullYear()}`
-    //document.getElementById('s-container').appendChild(newPost)
+    const id=localStorage.length()+1
+    const save={
+        "name":name,
+        "date":fdate,
+        "source":source,
+        "creator":"dummy",
+        "description":desc
+    }
+    localStorage.setItem(toString(id),save)
     console.log('created')
 }
 
 uploadbtn.onclick=createPost
+
+if (document.URL==`${document.domain}/website-test/`){
+    localStorage.forEach(v=>{
+        listScript(v.name,'tyler',v.date)
+    })
+}
