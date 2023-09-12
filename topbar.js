f1621819f538f60feadb0b488c070e2d728cc949
misspelled-usernaem <134952931+misspelled-usernaem@users.noreply.github.com>
2023-09-12 16:48:07 +0200

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -3,11 +3,11 @@ const scripts=document.getElementById('scripts-btn')
 const settings=document.getElementById('settings-btn')
 
 home.onclick=async()=>{
-    location.href=''
+    location.href='/website-test'
 }
 scripts.onclick=async()=>{
-    location.href=''
+    location.href='/website-test'
 }
 settings.onclick=async()=>{
-    location.href=''
+    location.href='/website-test'
 }
