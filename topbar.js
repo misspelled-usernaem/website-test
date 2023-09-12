00ed6c06de30e52a9d7fef09bb8d1aee95cc17b9
misspelled-usernaem <134952931+misspelled-usernaem@users.noreply.github.com>
2023-09-12 15:29:43 +0200

    Add files via upload

---
+++ topbar.js
@@ -0,0 +1,15 @@
+const home=document.getElementById('home-btn')
+const scripts=document.getElementById('scripts-btn')
+const settings=document.getElementById('settings-btn')
+
+const baseurl='file:///C:/Users/ist0923/OneDrive%20-%20Skellefte%C3%A5%20kommun/Desktop/JS/extension/'
+
+home.onclick=async()=>{
+    location.href=`${baseurl}/index.html`
+}
+scripts.onclick=async()=>{
+    location.href=`${baseurl}/index.html`
+}
+settings.onclick=async()=>{
+    location.href=`${baseurl}/index.html`
+}
---
+++ uploadscript.js
@@ -0,0 +1,21 @@
+const posttemp=document.getElementsByClassName('script-container')[0]
+const uploadbtn=document.getElementById('upload')
+createPost=async()=>{
+    //var newPost=posttemp.cloneNode(true)
+    //const c=newPost.childNodes
+    const name=document.getElementById('name').value
+    //const by=document.getElementById('').value
+    const source=document.getElementById('source').value
+    const desc=document.getElementById('description').value
+
+    const date=new Date
+
+
+    //c[0].value=name
+    //c[1].value=by
+    //c[2].value=`${date.getUTCDate()}/${date.getUTCDay()} - ${date.getUTCFullYear()}`
+    //document.getElementById('s-container').appendChild(newPost)
+    console.log('created')
+}
+
+uploadbtn.onclick=createPost
