312e954cc8c7fcc4a8a9e5438425a1427e8a59fc
misspelled-usernaem <134952931+misspelled-usernaem@users.noreply.github.com>
2023-09-12 15:31:51 +0200

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -2,7 +2,7 @@ const home=document.getElementById('home-btn')
 const scripts=document.getElementById('scripts-btn')
 const settings=document.getElementById('settings-btn')
 
-const baseurl='file:///C:/Users/ist0923/OneDrive%20-%20Skellefte%C3%A5%20kommun/Desktop/JS/extension/'
+const baseurl=document.baseURI
 
 home.onclick=async()=>{
     location.href=`${baseurl}/index.html`
